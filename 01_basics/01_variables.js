2fca36aa3fbe9686ce555e54946077a1a0257709
Anuj-yadav-5 <ramanuj356a@gmail.com>
2026-04-16 00:56:32 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 13234
+let accountEmail = "asjc@gmail.com"
+var accountPassword = "1234"
+
+accountCity = "Jaipur"
+
+//accountId = 2 // Not Allowed
+
+accountEmail = "sdfsf@gmail.com"
+accountPassword = "3343"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail ,accountPassword , accountCity])
