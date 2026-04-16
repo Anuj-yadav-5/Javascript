105c25ebd473d8515ece7d79ca9657af9a35ac62
Anuj-yadav-5 <ramanuj356a@gmail.com>
2026-04-16 21:46:47 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; 
+//treat all JS code as newer version
+
+//alert( 3 + 3)  we are using nodeJs , not browser
+
+
+
+let name = "Anuj"
+let age = 18
+let isLoggedIn = false
+let state = null
+let connect
+
+//number =>
+//bigint
+//String => ""
+//Boolean =>True/false
+//null => stndalone value
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof null)
+console.table([typeof name , typeof state , typeof age, typeof isLoggedIn , typeof connect])
